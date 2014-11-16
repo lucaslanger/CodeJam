b215f1f6790751953e2ed30f7fa7ed723e843727
Lucas Langer <lucaslangermont9@gmail.com>
2014-11-16 16:49:19 -0500

    fgf

--- helperfunctions.py
+++ helperfunctions.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 import numpy as np
 import pickle
-import os
+#import os
 
 from eyes import leftfacegraph
 from eyes import rightfacegraph
